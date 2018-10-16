ec6d0521a305c6c50c3dd6da98e703f61500c1f2
Christian Kaltepoth <christian@kaltepoth.de>
2018-10-16 09:51:44 +0200

    Typescript setup + first component

---
+++ src/If.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export type Props = {
+  condition: boolean;
+  children: React.ReactNode;
+}
+
+// Not a SFC because the typings are messed up
+// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/23422
+export class If extends React.Component<Props, {}> {
+  render(): React.ReactNode {
+    return this.props.condition ? this.props.children : null;
+  }
+}
